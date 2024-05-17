8528f19afc35dd1174d5e9c02f4da23a139a21a8
polarcatnean <nneann@gmail.com>
2024-05-17 14:47:26 +0700

    deleted test files

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure SQLAlchemy to connect to existing db
+# Configure SQLAlchemy to connect to existing
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout-log.db'
 db = SQLAlchemy(app)
 
