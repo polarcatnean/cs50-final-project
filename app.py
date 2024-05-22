8e8d64e361172ae04c42df9106113cb4775d4fcb
polarcatnean <nneann@gmail.com>
2024-05-22 13:30:10 +0700

    config webpack for bootstrap

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import datetime
 import pytz
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, get_flashed_messages, jsonify, redirect, render_template, request, send_from_directory, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, Date, DateTime, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from markupsafe import escape
 from helpers import login_required
 
 # Configure application
@@ -32,6 +32,8 @@ db = SQLAlchemy(model_class=Base)
 # Initialize the app with the extension
 db.init_app(app)
 print("Initialised the app.")
+
+# TODO use ruff or something else, formatter.
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -68,7 +70,8 @@ class WorkoutExercise(db.Model):
     reps: Mapped[int] = mapped_column(Integer, nullable=True)
     weight_kg: Mapped[float] = mapped_column(Float, nullable=False)  # 0 = bodyweight
     
-
+# TODO: put this in method that can be called during app initialization
+# https://flask.palletsprojects.com/en/3.0.x/patterns/appfactories/
 with app.app_context():
     print("Creating database tables...")
     db.create_all()
@@ -77,7 +80,6 @@ with app.app_context():
 
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
     # During development, you might want to disable caching to ensure that you're always seeing the most recent version of your site, not a cached version
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # tells the client's browser not to cache the response, not to store it, and to validate it
     response.headers["Expires"] = 0 #  sets the expiration date of the response to the past, which means it's already expired and can't be used from cache.
@@ -88,22 +90,42 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    user = db.session.scalars(db.select(User).filter_by(id=session["user_id"])).first()
+    return render_template("index.html", username=user.username)
 
+@app.route('/dist/<path:filename>')
+def dist(filename):
+    return send_from_directory('dist', filename)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """TODO Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
+    session.clear() # Forget any user_id >> this clears flashed messages also, use alert instead of flash
     if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username or not password:
+            flash("Please fill out both username and password.")
+            return redirect("/login")
+        user = db.session.scalars(db.select(User).filter_by(username=username)).first()
+        if not user:
+            flash("Username doesn't exist")
+            return redirect("/login")
+        if not check_password_hash(user.password_hash, password):
+            flash("Incorrect password")
+            return redirect("/login")
+        flash(f"signed in as <b>@{escape(user.username)}</b>!")
+        session["user_id"] = user.id
         return redirect("/")
-    else:
-        return render_template("index.html")
-    
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # clears session, effectively logging a user out.
+    session.clear()
+    flash("successfully signed out")
+    return redirect("/")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -112,6 +134,8 @@ def register():
         password = request.form.get("password")
         password_confirm = request.form.get("confirmation")
         email = request.form.get("email")
+        # TODO make checking parameters more succinct
+        # show all the errors at once, so the user can correct everything before submitting again
         if not username:
             flash("Please enter username.")
             return render_template("register.html")
@@ -124,7 +148,7 @@ def register():
         if password != password_confirm:
             flash("Passwords do not match.")
             return render_template("register.html")
-        # checks if username and email exist
+        # checks if username or email exist
         if db.session.execute(db.select(User).filter_by(username=username)).first():
             flash("This username already exists.")
             return render_template("register.html")
@@ -133,6 +157,7 @@ def register():
             return render_template("register.html")
 
         # hash password
+        # TODO use stronger hash method with scrypt.
         password_hash = generate_password_hash(password, method="pbkdf2", salt_length=16)
 
         # put in db
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
+'use strict'
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
   mode: 'development',
-  entry: '/script.js',
+  entry: './static/js/script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './templates/layout.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+      }
+    ]
   }
 };
