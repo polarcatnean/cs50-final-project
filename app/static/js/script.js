9cece96bef362026539356f9732b3f125cef373a
polarcatnean <nneann@gmail.com>
2024-05-23 18:22:31 +0700

    show log modal form when a date on calendar is clicked

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,14 +3,14 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 import '../css/styles.css'; // my custom css
-import { right } from '@popperjs/core';
+
 
 
 console.log("Hello from Webpack and Bootstrap!");
 
-
+// Calendar config
 document.addEventListener('DOMContentLoaded', function() {
   const calendarEl = document.getElementById('calendar')
   const calendar = new Calendar(calendarEl, {
@@ -26,36 +26,32 @@ document.addEventListener('DOMContentLoaded', function() {
         let progressBar = document.createElement("div");
         progressBar.className = "progress";
         progressBar.role = "progressbar";
-        progressBar.setAttribute('aria-label', 'Basic example');
-        progressBar.setAttribute('aria-valuenow', '55');
-        progressBar.setAttribute('aria-valuemin', '0');
-        progressBar.setAttribute('aria-valuemax', '100');
-        progressBar.style = "height: 20px";
 
         let innerDiv = document.createElement('div');
         innerDiv.className = 'progress-bar progress-bar-striped';
-        innerDiv.style = 'width: 55%';
-        innerDiv.innerText = '55%';
+        innerDiv.innerText = '65%';
 
         progressBar.appendChild(innerDiv);
         middleElement.appendChild(progressBar);
     },
     dateClick: function(info) {
-        let workout = prompt("log your workout: ");
-        if (workout) {
-            calendar.addEvent({
-                title: workout,
-                start: info.dateStr,
-                allDay: false,
-            })
-            info.dayEl.style.backgroundColor = "rgba(188,232,241,.3)";
-        }
+        // Trigger the Bootstrap modal
+        console.log(`${info.dateStr} is clicked`);
+        let logModal = new bootstrap.Modal(document.getElementById('log-modal'), { focus: true });
+        logModal.show();
+        // if (workout) {
+        //     calendar.addEvent({
+        //         title: workout,
+        //         start: info.dateStr,
+        //         allDay: false,
+        //     })
+        //     info.dayEl.style.backgroundColor = "rgba(188,232,241,.3)";
+        // }
     },
     selectable: false,
     unselectAuto: true
   })
   calendar.render();
-
 });
 
 
